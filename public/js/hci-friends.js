0f833b08c469e9e83649d3bf70406e32cbf3498f
Brian Yang (Mac Laptop) <brian@aerodroid.com>
2014-02-05 17:08:02 -0800

    Completed Lab 5

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	
+	$(".name_link").click(function(e) {
+		e.preventDefault();
+		var name = $(this).text();
+		$(this).text(anagrammedName(name));
+	});
 }
 
 function anagrammedName(name) {
